bb2451f7a0601aada179a0d4b3fed6a510b0f580
MarcoHuauya <marcohuauyalopez@gmail.com>
2023-10-01 15:34:07 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appwebfragmets2/AppWebFragmets2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragmets2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFragmets2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFragmets2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebfragmets2/AppWebFragmets2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragmets2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragmets2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
